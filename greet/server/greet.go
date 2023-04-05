4951700a547904b4e3f4d45101539abbfb8bf8a8
Renato Spakauskas <renatospaka@gmail.com>
2023-04-05 11:12:46 -0300

    refactor: :recycle: gRPC  client
    spliting the code into new file greet

---
+++ greet/server/greet.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/renatospaka/grpc-calculator/greet/proto"
+)
+
+func (s *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
+	log.Printf("Greet function invoked with %v\n", in)
+
+	return &pb.GreetResponse{
+		Result: "Hello " + in.FirstName,
+	}, nil
+}
