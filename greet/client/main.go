505d77dbfc31c44bb6f8f6272ec268c54fe693e2
Renato Spakauskas <renatospaka@gmail.com>
2023-04-06 13:54:35 -0300

    feat: :sparkles: unary gRPC
    very basic unary communication

--- greet/client/main.go
+++ greet/client/main.go
@@ -5,6 +5,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/renatospaka/grpc-calculator/greet/proto"
 )
 
 var addr string = "localhost:50051"
@@ -16,5 +17,6 @@ func main() {
 	}
 	defer conn.Close()
 
-
+	c := pb.NewGreetServiceClient(conn)
+	doGreet(c)
 }
